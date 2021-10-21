e989cc628356960ae6664ac1a414788f276f0d6d
Theo van den Heuvel <vdheuvel@heuvelhlt.nl>
2021-10-21 14:08:46 +0200

    add pictures

--- index.php
+++ index.php
@@ -34,7 +34,7 @@ $menuPages = array(
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title> Untangle </title>
+  <title><?php echo $company; ?></title>
   <link rel="stylesheet" href="<?php
       if(strpos($_SERVER['REQUEST_URI'], 'tempWebsite') !== false) {
         echo '/tempWebsite';
