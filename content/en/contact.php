fa3b38c672284d398f7711309cc3ade51a6e8858
Irene van den Heuvel <irene@heuvelhlt.nl>
2021-10-29 16:46:47 +0200

    give each content file a description

--- content/en/contact.php
+++ content/en/contact.php
@@ -76,7 +76,8 @@ if (!empty($_POST['token']) && !empty($_SESSION[$_POST['token']])) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>contact form</title>
+    <title>Contact form</title>
+    <meta name="description" content="Contact us when you want to know more about our services ">
   </head>
   <body>
     
--- content/nl/contact.php
+++ content/nl/contact.php
@@ -89,7 +89,8 @@ if (!empty($_POST['token']) && !empty($_SESSION[$_POST['token']])) {
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>contact</title>
+  <title>Contact formulier</title>
+  <meta name="description" content="Neem contact met ons op indien u meer wilt weten over onze services">
 </head>
 <body>
   
