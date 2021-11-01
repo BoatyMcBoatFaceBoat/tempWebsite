723e732b11201529e66812c49706d623ad30c871
paulroefsuntangle <paulroefs@heuvelhlt.nl>
2021-11-01 11:22:25 +0100

    fix links and languages

--- index.php
+++ index.php
@@ -1,9 +1,20 @@
 <?php
 $company = "Untangle Data";
-$lang = 'nl';
+$langDefault = 'nl';
+$prefix = '';
+if (strpos($_SERVER['REQUEST_URI'], 'tempWebsite')) {
+  $prefix = '/tempWebsite';
+}
+$prefixLang = $prefix;
 $page = 'home';
 if (!empty($_GET['lang'])) {
+  if($_GET['lang'] === $langDefault) {
+    header('Location: ' . $prefix . '/' . (!empty($_GET['page']) ? $_GET['page'] : ''));
+  }
   $lang = $_GET['lang'];
+  $prefixLang .= '/' . $lang;
+} else {
+  $lang = $langDefault;
 }
 if (!empty($_GET['page'])) {
   $page = $_GET['page'];
@@ -25,9 +36,32 @@ $menuPages = array(
   array('consultancy' => array('model', 'language')),
   array('software' => array('electron', 'raku'))
 );
-$prefix = '';
-if (strpos($_SERVER['REQUEST_URI'], 'tempWebsite')) {
-  $prefix = '/tempWebsite';
+
+$misspage = '';
+if (!empty($pages->$page)) {
+  if (!empty($pages->$page->lang->$lang)) {
+    $currentPageFile = 'content/' . $lang . '/' . $pages->$page->filename;
+  } else {
+    switch ($lang) {
+      case "nl":
+        $misspage = 'Deze pagina bestaat niet in uw taal.';
+        break;
+      case "en":
+        default:
+        $misspage = 'this page does not exist in your language';
+        break;
+    }
+  }
+} else {
+  switch ($lang) {
+    case "nl":
+      $misspage = 'Deze pagina bestaat niet.';
+      break;
+    case "en":
+      default:
+      $misspage = 'this page does not exist';
+      break;
+  }
 }
 ?>
 <html>
@@ -39,6 +73,28 @@ if (strpos($_SERVER['REQUEST_URI'], 'tempWebsite')) {
     <!-- <meta name="google-site-verification" content="+nxGUDJ4QpAZ5l9Bsjdi102tLVC21AIh5d1Nl23908vVuFHs34="/> -->
     
     <title> Untangle </title>
+      <?php
+      if(isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on')   {
+        $url = "https://";   
+      } else {
+        $url = "http://";
+      }
+      $url.= $_SERVER['HTTP_HOST'];   
+      foreach ($languages as $language) {
+        if ($language != $lang) {
+            echo '<link rel="alternate" hreflang="' . $language . '" href="';
+            echo $url . $prefix . '/';
+            if($language !== $langDefault) {
+              echo $language . '/' ;
+            }
+            if(!empty($currentPageFile)) {
+              echo $page;
+            }
+            echo '">';
+        }
+      }
+      ?>
+    <!-- <link rel="alternate" hreflang="en" href="https://heuvelhlt.nl/en/consultancy" /> -->
     <link rel="stylesheet" href="<?php echo $prefix ?>/styles/style.css">
     <script src="<?php echo $prefix ?>/script/untangle.js"></script>
     <script src="<?php echo $prefix ?>/script/temp.js"></script>
@@ -47,11 +103,7 @@ if (strpos($_SERVER['REQUEST_URI'], 'tempWebsite')) {
 <body onload="loadWebsite()">
 <header>
     <div class="left-header-items"></div>
-    <a href="/<?php echo $prefix;
-    if (!empty($_GET['lang'])) {
-      echo $lang . '/';
-    }
-    ?>"><img src="<?php echo $prefix ?>/media/untangle_logo_light.png" alt="untangle data logo" class="logo"/></a>
+    <a href="<?php echo $prefixLang; ?>/"><img src="<?php echo $prefix ?>/media/untangle_logo_light.png" alt="untangle data logo" class="logo"/></a>
     <div class="header-menu-icon">
       <span class="hamburger">
         <span class="top"></span>
@@ -84,7 +136,7 @@ if (strpos($_SERVER['REQUEST_URI'], 'tempWebsite')) {
                     }
                     echo '"';
                   }
-                  echo '><a href="' . $menuPage . '">' . $pages->$menuPage->lang->$lang . $openTrigger . '</a>';
+                  echo '><a href="' . $prefixLang . '/' . $menuPage . '">' . $pages->$menuPage->lang->$lang . $openTrigger . '</a>';
                   if (!empty($menuPageArray)) {
                     echo '<ul>';
                     foreach ($menuPageArray as $subMenuPage) {
@@ -92,7 +144,7 @@ if (strpos($_SERVER['REQUEST_URI'], 'tempWebsite')) {
                       if ($page === $subMenuPage) {
                         echo ' class="active"';
                       }
-                      echo '><a href="' . $subMenuPage . '">' . $pages->$subMenuPage->lang->$lang . '</a>';
+                      echo '><a href="' . $prefixLang . '/' . $subMenuPage . '">' . $pages->$subMenuPage->lang->$lang . '</a>';
                     }
                     echo '</ul>';
                   }
@@ -112,8 +164,13 @@ if (strpos($_SERVER['REQUEST_URI'], 'tempWebsite')) {
         if ($language != $lang) {
           ?>
             <a class="lang-item" href="<?php
-            echo $prefix;
-            echo '/' . $language . '/';
+            echo $prefix . '/';
+            if($language !== $langDefault) {
+              echo $language . '/' ;
+            }
+            if(!empty($currentPageFile)) {
+              echo $page;
+            }
             ?>">
                 <img src="<?php echo $prefix ?>/media/flag_<?php echo $language; ?>.png"/>
             </a>
@@ -125,11 +182,11 @@ if (strpos($_SERVER['REQUEST_URI'], 'tempWebsite')) {
 </header>
 
 <div class="breadCrumb"><?php
-  $breadcrumb = '<a href="' . $page . '">' . $pages->$page->lang->$lang . '</a>';
+  $breadcrumb = '<a href="' . $prefixLang . '/' . $page . '">' . $pages->$page->lang->$lang . '</a>';
   $breadcrumbPage = $page;
   while ($pages->$breadcrumbPage->parent != false) {
     $breadcrumbPage = $pages->$breadcrumbPage->parent;
-    $breadcrumb = '<a href="' . $breadcrumbPage . '">' . $pages->$breadcrumbPage->lang->$lang . '</a> / ' . $breadcrumb;
+    $breadcrumb = '<a href="' . $prefixLang . '/' . $breadcrumbPage . '">' . $pages->$breadcrumbPage->lang->$lang . '</a> / ' . $breadcrumb;
   }
   echo $breadcrumb;
   ?>
@@ -137,38 +194,12 @@ if (strpos($_SERVER['REQUEST_URI'], 'tempWebsite')) {
 
 <div class="content-wrapper">
     <div class="content"><?php
-      if (!empty($pages->$page)) {
-        if (!empty($pages->$page->lang->$lang)) {
-          // echo "Deze pagina is wel beschikbaar in uw taalversie.";
-          include('content/' . $lang . '/' . $pages->$page->filename);
-        } else {
-          $misspage = '';
-          switch ($lang) {
-            case "nl":
-              $misspage = 'Deze pagina bestaat niet in uw taal.';
-              break;
-            case "en":
-              default:
-              $misspage = 'this page does not exist in your language';
-              break;
-          }
-          echo $misspage;
-        }
-      } else {
-        $misspage = '';
-        switch ($lang) {
-          case "nl":
-            $misspage = 'Deze pagina bestaat niet.';
-            break;
-          case "en":
-            default:
-            $misspage = 'this page does not exist';
-            break;
-        }
-        echo $misspage;
-
-      }
-      if ($page != 'contact') {
+    if(!empty($currentPageFile)) {
+      include($currentPageFile);
+    } else {
+      echo $misspage;
+    }
+    if (!empty($currentPageFile) && $page != 'contact') {
         ?>
           <!-- theo: contact-element should not be loaded in case $page == 'contact' -->
           <div class="contact-element">
@@ -208,7 +239,7 @@ if (strpos($_SERVER['REQUEST_URI'], 'tempWebsite')) {
         <ul>
           <?php
           foreach ($aboutPages as $aboutPage) {
-            echo '<li><a href="' . $aboutPage . '"';
+            echo '<li><a href="' . $prefixLang . '/' .$aboutPage . '"';
             if ($page === $aboutPage) {
               echo ' class="active"';
             }
